b2ca2b5648178481407e5d411776c973e3f9f73e
danielcorte <daniellcorte@gmail.com>
2025-01-20 20:54:05 -0300

    exercicio 2 de for

--- src/section6repetitiveStructure/exercicios/ExFor2.java
+++ src/section6repetitiveStructure/exercicios/ExFor2.java
@@ -1,5 +1,7 @@
 package section6repetitiveStructure.exercicios;
 
+import java.util.Scanner;
+
 public class ExFor2 {
 
     public static void main(String[] args) {
@@ -14,5 +16,25 @@ public class ExFor2 {
         (use a palavra "in" para dentro do intervalo,
         e "out" para fora do intervalo)
          */
+        int escolha, in = 0, out = 0, valor;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos valores deseja inserir?: ");
+        escolha = sc.nextInt();
+
+        for (int i = 0; i < escolha; i++) {
+            System.out.printf("Insira o %d° valor: ", i + 1);
+            valor = sc.nextInt();
+
+            if (valor >= 10 && 20 <= valor) {
+                in += 1;
+            } else {
+                out += 1;
+            }
+
+        }
+        System.out.println(in + " in");
+        System.out.println(out + " out");
     }
 }
