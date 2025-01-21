2326ade4e919055e574f10555d815fe8e5780b04
danielcorte <daniellcorte@gmail.com>
2025-01-20 23:07:22 -0300

    exercicio 5 de for

--- src/section6repetitiveStructure/exercicios/ExFor5.java
+++ src/section6repetitiveStructure/exercicios/ExFor5.java
@@ -1,10 +1,28 @@
 package section6repetitiveStructure.exercicios;
 
+import java.util.Scanner;
+
 public class ExFor5 {
 
+    public static void main(String[] args) {
     /*
-    Ler um valor N. Calcular e escrever seu respectivo fatorial.
+    Ler um valor N. Calcular e escrever o seu respetivo fatorial.
     Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
     Lembrando que, por definição, fatorial de 0 é 1.
     */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Deseja saber o fatorial de qual numero?: ");
+        int num = sc.nextInt();
+
+        int fatorial = 1;
+        for (int i = 1; i <= num; i++) {
+            fatorial = fatorial * i;
+        }
+
+        System.out.println(fatorial);
+
+        sc.close();
+    }
+
 }
