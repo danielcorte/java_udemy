b18ec3e86c35e1364a48eaf3de230b1e657adde1
danielcorte <daniellcorte@gmail.com>
2025-01-20 21:20:14 -0300

    exercicio 4 de for

--- src/section6repetitiveStructure/exercicios/ExFor4.java
+++ src/section6repetitiveStructure/exercicios/ExFor4.java
@@ -1,5 +1,7 @@
 package section6repetitiveStructure.exercicios;
 
+import java.util.Scanner;
+
 public class ExFor4 {
 
     public static void main(String[] args) {
@@ -8,8 +10,28 @@ public class ExFor4 {
         Fazer um programa para ler um número N.
         Depois leia N pares de números e mostre
         a divisão do primeiro pelo segundo. Se
-        o denominador for igual a zero, mostrar
+        o denominador igualar zero, mostrar
         a mensagem "divisao impossivel".
          */
+
+        Scanner sc = new Scanner(System.in);
+
+        int escolha;
+
+        System.out.print("Deseja a divisao de quantos valores?: ");
+        escolha = sc.nextInt();
+
+        for (int i = 0; i < escolha; i++) {
+
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+
+            if (num2 == 0) {
+                System.out.println("Divisão impossível!!");
+            } else {
+                double resultado = num1 / num2;
+                System.out.println(resultado);
+            }
+        }
     }
 }
