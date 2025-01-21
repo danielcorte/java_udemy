91397cec4ac315b6b14aa579c4c08ca88637f95b
danielcorte <daniellcorte@gmail.com>
2025-01-20 23:12:09 -0300

    exercicio 6 de for

--- src/section6repetitiveStructure/exercicios/ExFor6.java
+++ src/section6repetitiveStructure/exercicios/ExFor6.java
@@ -1,5 +1,7 @@
 package section6repetitiveStructure.exercicios;
 
+import java.util.Scanner;
+
 public class ExFor6 {
 
     public static void main(String[] args) {
@@ -7,5 +9,17 @@ public class ExFor6 {
         /*
         Ler um número inteiro N e calcular todos os seus divisores.
         */
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.print("Insira um valor e eu lhe darei todos os divisores: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+
+            if (num % i == 0) System.out.println("Divisor encontrado: " + i);
+        }
     }
 }
