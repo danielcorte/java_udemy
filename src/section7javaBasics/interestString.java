e549fa3e30fe3a2060b31721535b2c1b2e0f3f7b
danielcorte <daniellcorte@gmail.com>
2025-01-24 11:25:58 -0300

    some changes

---
+++ src/section7javaBasics/interestString.java
@@ -0,0 +1,18 @@
+package section7javaBasics;
+
+public class interestString {
+
+    public static void main(String[] args) {
+
+        /*
+        Formatar: toLowerCase(), toUpperCase(), trim()
+        Recortar: substring(inicio), substring(inicio, fim)
+        Substituir: Replace(char, char), Replace(string, string)
+        Buscar: IndexOf, LasIndexOf
+        str.Split("")
+         */
+
+        String original = "abcde FGHIJ ABC abc DEFG";
+        String
+    }
+}
