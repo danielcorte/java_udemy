a71d5e837893b93fba75bdace21dcba674be067f
danielcorte <daniellcorte@gmail.com>
2025-01-26 23:56:39 -0300

    string tips

--- src/section7javaBasics/interestString.java
+++ src/section7javaBasics/interestString.java
@@ -12,7 +12,23 @@ public class interestString {
         str.Split("")
          */
 
-        String original = "abcde FGHIJ ABC abc DEFG";
-        String
+        String original = "abcde FGHIJ ABC abc DEFG    ";
+
+        String s01 = original.toLowerCase();
+        String s02 = original.toUpperCase();
+        String s03 = original.trim();
+        String s04 = original.substring(2);
+        String s05 = original.substring(2, 8);
+        String s06 = original.replace('a', 'x');
+        String s07 = original.replace("abc", "xyz");
+
+        System.out.println("Original: -" + original + "-");
+        System.out.println("toLowerCase: -" + s01 + "-");
+        System.out.println("toUpperCase: -" + s02 + "-");
+        System.out.println("trim: -" + s03 + "-");
+        System.out.println("substring: -" + s04 + "-");
+        System.out.println("substring2: -" + s05 + "-");
+        System.out.println("replace('a', 'x'): -" + s06 + "-");
+        System.out.println("replace('abc', 'xyz'): -" + s07 + "-");
     }
 }
