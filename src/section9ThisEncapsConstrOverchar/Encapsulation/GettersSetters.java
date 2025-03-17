a4ae25cffe696dd22961fdc3a6aea83855a38d39
danielcorte <daniellcorte@gmail.com>
2025-03-17 16:57:06 -0300

    encapsulamento

---
+++ src/section9ThisEncapsConstrOverchar/Encapsulation/GettersSetters.java
@@ -0,0 +1,28 @@
+package section9ThisEncapsConstrOverchar.Encapsulation;
+
+public class GettersSetters {
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/section9ThisEncapsConstrOverchar/Encapsulation/Main.java
@@ -0,0 +1,38 @@
+package section9ThisEncapsConstrOverchar.Encapsulation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        Encapsulamento:
+
+        - É um princípio que consiste em esconder detalhes
+        de implementação de uma classe, expondo apenas
+        operações seguras e que mantenham os objetos em um
+        estado consistente.
+
+        - Regra de Ouro: o objeto deve sempre estar em um
+        estado consistente, e a própria classe deve garantir
+        isso.
+
+        Analogia: Aparelho de som
+            - Existem vários circuitos e operações ocorrendo dentro do aparelho
+
+            - O aparelho esconde tudo isso, e deixa a mostra para o usuário somente
+              operações que não irão corromper a integridade do aparelho
+
+          Exemplo:
+            - Tocar musica
+            - Inserir CD
+            - Pausar
+        */
+
+        /*
+        Regra geral:
+        - Um objeto não deve expor nenhum atributo (modificador de acesso private)
+
+        - Os atributos devem ser acessados por meio de metodos GET e SET
+         */
+    }
+}
